86f4be59ff429f969c6987187513c32085f0bd6e
montassar <2130091@isi-montreal.com>
2023-04-27 13:21:31 -0400

    page recette avec evaluation avec description avec image

--- KIWI2/frontTest/foodapp/src/App.js
+++ KIWI2/frontTest/foodapp/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 // import components
 import Hero from './components/Hero';
-/*import About from './components/About';
-import Menu from './components/Menu';
-
-import Team from './components/Team';
-import Testimonial from './components/Testimonial';
-import Reservation from './components/Reservation';*/
+import NavLinks from './components/NavLinks';
+import About from './components/About';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className='h-full bg-pattern bg-repeat max-w-[1800px] mx-auto overflow-hidden'>
-    
       <Hero />
+      <NavLinks />
+      <About />
+      {/* 
       
-     {/* <About />
       <Menu />
-      <Team /> 
+      <Team />
       <Testimonial />
-  <Reservation />*/}
-      <Footer />
+      <Reservation />*/}
+      <Footer /> 
       <div className='h-[380px] md:h-[370px]'></div>
     </div>
   );

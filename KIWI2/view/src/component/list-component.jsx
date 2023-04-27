0519a73a95017600e33708aa1595495869ba9029
Jean Waston Therane <89857594+jewathe@users.noreply.github.com>
2023-04-27 14:29:46 -0400

    fonctionnalite ajouter une recette ok

--- KIWI2/view/src/component/list-component.jsx
+++ KIWI2/view/src/component/list-component.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 function renderLi (recipe, onItemClick, onItemDeleteClick) {
     return (
-        <li key={recipe.id} id={recipe.id} title={'ID:' + recipe.id}>
+        <div key={recipe.id} id={recipe.id} title={'ID:' + recipe.id}>
             <span onClick={onItemClick}>
                 <strong>{recipe.proposedtitle}</strong> : {recipe.proposeddescription} {recipe.adoptedtitle} {recipe.adopteddescription} {'depuis ' + recipe.createdat + ' '}
             </span>
             <button onClick={onItemDeleteClick}>X</button>
-        </li>
+        </div>
     )
 }
 
 const ListComponent = ({ recipes, onItemClick, onItemDeleteClick, onAddClick, onAddClick1 }) => (
     <div>
-        <ul>
+        <div>
             {recipes.map((recipe) => renderLi(recipe, onItemClick, onItemDeleteClick))}
-        </ul>
+        </div>
         <button onClick={onAddClick}>Ajouter une recette</button>
         <button onClick={onAddClick1}>Ajouter un ingredient</button>
     </div>

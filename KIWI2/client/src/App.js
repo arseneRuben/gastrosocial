880ecad34d0039a80dd576ef10ab1aeb362bcf59
Jean Waston Therane <89857594+jewathe@users.noreply.github.com>
2023-05-02 10:37:36 -0400

    Nouveau client pour adapter les fonctionnalites

--- KIWI2/client/src/App.js
+++ KIWI2/client/src/App.js
@@ -1,30 +1,23 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homePage";
-import AuthPage from "./pages/authPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import Header from "./component/partials/Header";
-import RecipePage from "./pages/recipePage/index";
-import ProfilePage from "./pages/profilePage";
-import IngredientPage from "./pages/ingredientPage";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <Header/>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/ingredient/index" element={<IngredientPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/recipe/index" element={<RecipePage />} />
-        </Routes>
-      </BrowserRouter>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

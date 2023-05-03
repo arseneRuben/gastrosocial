9703896029e19882b2fe77397d1dac77736d32d1
Arsene Fokam P <2210100@isi-montreal.com>
2023-05-03 05:40:07 -0400

    creation de  recettes

--- KIWI2/client/src/api/index.js
+++ KIWI2/client/src/api/index.js
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: `http://localhost:8000` });
 
-
 export const fetchRecipe = (id) => API.get(`/recipes/${id}`);
 export const fetchRecipes = () => API.get(`/recipes`);
 export const fetchRecipesByCreator = (name) => API.get(`/recipes/creator?name=${name}`);
 export const fetchRecipesBySearch = (searchQuery) => API.get(`/recipes/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
-export const createRecipe = (newRecipe) => API.post('/recipes', newRecipe);
+export const createRecipe = (newRecipe) => API.post(`/recipes`, newRecipe);
 export const likeRecipe = (id) => API.patch(`/recipes/${id}/likeRecipe`);
 export const comment = (value, id) => API.recipe(`/recipes/${id}/commentRecipe`, { value });
 export const updateRecipe = (id, updatedRecipe) => API.patch(`/recipes/${id}`, updatedRecipe);
--- KIWI2/server/dao/recipe.js
+++ KIWI2/server/dao/recipe.js
@@ -2,39 +2,60 @@ import { connect, query, disconnect } from '../dao/dao.js'
 import { CONTENT_TYPE_JSON, HTTP_OK, writeJSONResponse } from './util.js'
 
 export const getRecipes = async (req, res) => {
-    connect()
-    query('SELECT * FROM recipe ', [], (result) => {
-        res.writeHead(HTTP_OK, { 'Content-Type': CONTENT_TYPE_JSON })
-        res.end(JSON.stringify(result.rows, null, 4))
-        disconnect()
-    })
+    try {
+        connect()
+        query('SELECT * FROM recipe ', [], (result) => {
+            res.writeHead(HTTP_OK, { 'Content-Type': CONTENT_TYPE_JSON })
+            res.end(JSON.stringify(result.rows, null, 4))
+            disconnect()
+        })
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
 
 export const deleteRecipe = async (req, res) => {
-    connect()
-    query('DELETE  FROM recipe WHERE id=$1', [req.params.id], (result) => {
-        res.writeHead(HTTP_OK, { 'Content-Type': CONTENT_TYPE_JSON })
-        disconnect()
-    })
+    try {
+        connect()
+        query('DELETE  FROM recipe WHERE id=$1', [req.params.id], (result) => {
+            res.writeHead(HTTP_OK, { 'Content-Type': CONTENT_TYPE_JSON })
+            disconnect()
+        })
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
 
 export const getRecipe = async (req, res) => {
-    connect()
-    query('SELECT * FROM recipe WHERE id=$1', [req.params.id], (resp) => {
-        writeJSONResponse(req, res, resp.rows)
-        disconnect()
-    })
+    try {
+        connect()
+
+        query('SELECT * FROM recipe WHERE id=$1', [req.params.id], (resp) => {
+            writeJSONResponse(req, res, resp.rows)
+            disconnect()
+        })
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
 
 export const createRecipe = async (req, res) => {
-    connect()
-    query('INSERT INTO recipe (proposed_title, proposed_description,user_id,preparation_time,cooking_time,portions) VALUES ($1, $2, $3, $4, $5,$6)', [req.body.proposed_title, req.body.proposed_description, req.body.user_id, req.body.preparation_time, req.body.cooking_time, req.body.portions], function () {
-        disconnect()
-    })
+    try {
+        connect()
+        query('INSERT INTO recipe (proposed_title, proposed_description,user_id,preparation_time,cooking_time,portions) VALUES ($1, $2, $3, $4, $5,$6)', [req.body.proposed_title, req.body.proposed_description, req.body.user_id, req.body.preparation_time, req.body.cooking_time, req.body.portions], function () {
+            disconnect()
+        })
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
 export const updateRecipe = async (req, res) => {
-    connect()
-    query('UPATE  recipe SET proposed_title= $1 , proposed_description=$2,user_id=$3,preparation_time=$4,cooking_time=$5,portions=$6 WHERE id=$7', [req.body.proposed_title, req.body.proposed_description, req.body.user_id, req.body.preparation_time, req.body.cooking_time, req.body.portions, req.body.id], function () {
-        disconnect()
-    })
+    try {
+        connect()
+        query('UPATE  recipe SET proposed_title= $1 , proposed_description=$2,user_id=$3,preparation_time=$4,cooking_time=$5,portions=$6 WHERE id=$7', [req.body.proposed_title, req.body.proposed_description, req.body.user_id, req.body.preparation_time, req.body.cooking_time, req.body.portions, req.body.id], function () {
+            disconnect()
+        })
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
